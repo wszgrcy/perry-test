0c6b51ff3c132ce9e2b44788fc6eeb7111456ae8
chen <wszgrcy@gmail.com>
2026-05-11 22:20:38 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import * as v from 'valibot';
 function main(): void {
   console.log('Hello from Perry!');
-  let ob1 = v.object({ a1: v.string() });
   // success
   console.log('123');
   // success
   console.log('123' === '123');
-  // failed 123 on js
-  console.log(v.parse(ob1, { a1: '123' }).a1);
-  // failed true on js
-  console.log(v.parse(ob1, { a1: '123' }).a1 === '123');
-  // failed {"a1":"123"} on js
-  console.log(JSON.stringify(v.parse(ob1, { a1: '123' })));
+  // failed 123 on js / [object Object] on perry
+  console.log(v.parse(v.string(), '123'));
+  // failed true on js /false on perry
+  console.log(v.parse(v.string(), '123') === '123');
+  // failed "123" on js / "" on  perry
+  console.log(JSON.stringify(v.parse(v.string(), '123')));
   console.log('end');
 }
 
