813ea58e08f7c1643ec876b3a64067fac36335b6
chen <wszgrcy@gmail.com>
2026-05-13 09:43:29 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
-import * as v from 'valibot';
+// import * as v from 'valibot';
+import {} from '@piying/valibot-visit';
 function main(): void {
-  console.log('Hello from Perry!');
-  // success
-  console.log('123');
-  // success
-  console.log('123' === '123');
-  // failed 123 on js / [object Object] on perry
-  console.log(v.parse(v.string(), '123'));
-  // failed true on js /false on perry
-  console.log(v.parse(v.string(), '123') === '123');
-  // failed "123" on js / "" on  perry
-  console.log(JSON.stringify(v.parse(v.string(), '123')));
-  console.log('end');
+  // console.log('Hello from Perry!');
+  // // success
+  // console.log('123');
+  // // success
+  // console.log('123' === '123');
+  // // failed 123 on js / [object Object] on perry
+  // console.log(v.parse(v.string(), '123'));
+  // // failed true on js /false on perry
+  // console.log(v.parse(v.string(), '123') === '123');
+  // // failed "123" on js / "" on  perry
+  // console.log(JSON.stringify(v.parse(v.string(), '123')));
+  // console.log('end');
+  // class TestHandle extends BaseSchemaHandle<any> {}
+  // let result = convertCore(
+  //   v.string(),
+  //   (item) => {
+  //     return item.type;
+  //   },
+  //   {
+  //     handle: TestHandle,
+  //   },
+  // );
+  // console.log(result);
+  // console.log(result === 'string');
 }
 
 main();
